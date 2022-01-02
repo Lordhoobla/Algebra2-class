19513351593f963629c57d03a27b06616a4b6239
Lordhoobla <45267209+Lordhoobla@users.noreply.github.com>
2022-01-02 11:36:22 -0600

    Update Extra.java

--- Extra.java
+++ Extra.java
@@ -1,5 +1,12 @@
 //This is for all the extra math calculations that don't fit into the otehrs
 public class Extra{
+  /** This will take a number and retunr the optimal square for that number
+  * @param numOf - The purpose of this number is to serve as the total number of positions that need to be assigned
+  * @return int[] 
+  	- the first number will be the row count
+	- The second number will be the column count
+	- The third number will be how many will be left out from the square to use for either an additional row or column
+  */
   public static int[] gridFind(int numOf){
     int bet=0;
 		while(Algebra.isPrime(numOf)){bet++;numOf--;}
